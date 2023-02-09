4d1faed096181ab74a012a52a21c423b88bf2103
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-02-09 15:24:32 -0400

    atualização

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+
+  extends: ["standard", "prettier"],
+
+  Plugin: ["prettier"],
+
+  overrides: [],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    camelcase: "off",
+    "prettier/prettier": "error",
+  },
+};
