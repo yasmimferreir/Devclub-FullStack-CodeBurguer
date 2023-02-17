27dcc4ea56fb75bf03ad186fa8554308239bfe50
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-02-17 15:45:08 -0400

    test connection BD / project CodeBurguer

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,20 +1,48 @@
 import { v4 } from "uuid";
 import User from "../models/User";
+import * as Yup from "yup";
 
 class UserController {
   async store(request, response) {
-    const { name, email, password_hash, admin } = request.body;
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    });
+
+    /*if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Error validation" });
+    } */
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { name, email, password, admin } = request.body;
+
+    const userExists = await User.findOne({
+      where: { email },
+    });
+
+    if (userExists) {
+      return response.status(400).json({ error: "Esse usuario já existe" });
+    }
+
+    console.log(userExists);
 
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     });
 
-   return response.status(201).json({ id: user.id, name, email, admin})
+    return response.status(201).json({ id: user.id, name, email, admin });
   }
 }
 
-export default new UserController()
+export default new UserController();
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from "sequelize";
+import bcrypt from "bcrypt";
 
 class User extends Model {
   static init(sequelize) {
@@ -6,12 +7,20 @@ class User extends Model {
 
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         admin: Sequelize.BOOLEAN
     }, {
-        sequelize
-
+        sequelize,
     })
+
+    this.addHook('beforeSave', async(user) => {
+      if(user.password){
+        user.password_hash = await bcrypt.hash(user.password, 10)
+      }
+    })
+
+    return this
   }
 }
 
