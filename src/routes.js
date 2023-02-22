df5179137a35b0795e5c6911bc1477334baee85b
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-02-22 13:18:38 -0400

    atualização/ProjetoCodeBurguer

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import ProductController from "./app/controllers/ProductController";
 import Sessioncontroller from "./app/controllers/Sessioncontroller";
 import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 routes.post("/users", UserController.store);
 routes.post("/sessions", Sessioncontroller.store);
+routes.post("/products", ProductController.store);
 
 export default routes;
